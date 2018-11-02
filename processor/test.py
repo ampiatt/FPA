c41670588541dcb938c6cfdfbf762c169cbb7a0c
Gavin Stark <gstark31897@gmail.com>
2018-11-02 17:55:34 -0500

    adding initial processor code

---
+++ processor/test.py
@@ -0,0 +1,27 @@
+import gensim
+import tensorflow as tf
+import csv
+
+def sentences():
+    with open('Combined_News_DJIA.csv') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            i = 0
+            for title in row:
+                if i < 2:
+                    i += 1
+                    continue
+                yield [item for item in title.split()]
+
+data = []
+for sentence in sentences():
+    data.append(sentence)
+data = data[25:]
+model = gensim.models.Word2Vec(data, min_count=1)
+
+print(model.wv.similarity("Russia", "NATO"))
+print(model.wv.similarity("DJIA", "NATO"))
+print(model.wv.similarity("DJIA", "Dogs"))
+
+for word in data[0]:
+    print('{}: {}'.format(word, model.wv.similarity('DJIA', word)))
